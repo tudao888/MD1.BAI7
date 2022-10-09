ec20d2f10a01aee95b25979f8c74bf25460cf446
tudao888 <daotu2106@gmail.com>
2022-10-09 21:25:53 +0700

    chuyendoitiente

---
+++ dientichhinhtron.js
@@ -0,0 +1,4 @@
+inputbankinh = prompt('Nhap vao ban kinh')
+var R = parseInt(inputbankinh)
+var S = 3.14 * R**2
+document.write("S:" + S)
---
+++ index2.js
@@ -0,0 +1,12 @@
+inputdoC = prompt('NhapvaodoC')
+var doC = parseInt(inputdoC)
+var doF = 9 * doC / 5 + 32
+document.write("Do Fahrenheit:" + doF)
+
+
+// inputwidth = prompt("Enter the width")
+// inputheight = prompt("Enter the height")
+// var width = parseInt(inputwidth)
+// var height = parseInt(inputheight)
+// var area = width * height
+// document.write('The area is:' + area)
